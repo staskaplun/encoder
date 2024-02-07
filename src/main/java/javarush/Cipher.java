f01d07dcdda1f51a66c65deeae270ce255c6ad6e
Stas <staskaplun@gmail.com>
2024-02-08 01:23:08 +0200

    two

--- src/main/java/javarush/Cipher.java
+++ src/main/java/javarush/Cipher.java
@@ -1,13 +1,31 @@
 package src.main.java.javarush;
 
+import java.util.ArrayList;
+
 public class Cipher {
-    public String textin;
-    public int key;
 
-    public String encript(String text, int key){
-        char[] textarray = text.toCharArray();
-        for (int i = 0; i < textarray.length; i++) {
-
+    private char getLetterShift(char letter, int shift){
+        Alphabet alphabet = new Alphabet();
+        ArrayList<Character> alphabetLis = alphabet.getAlphabetList();
+        int index = alphabetLis.indexOf(letter);
+        if (index == -1){
+            return letter;
         }
+        else {
+            return alphabetLis.get(index + shift);
+        }
+    }
+
+    public String getEncrypt(String text, int key){
+        String textencrypt = "";
+       char[] textarray = text.toCharArray();
+       for (char letter: textarray){
+           textencrypt = textencrypt + getLetterShift(letter, key);
+       }
+       return textencrypt;
+    }
+
+    public String getDecrypt(String text, int key){
+        return getEncrypt(text, -key);
     }
 }
--- src/main/java/javarush/Test.java
+++ src/main/java/javarush/Test.java
@@ -2,10 +2,23 @@ package src.main.java.javarush;
 
 public class Test {
 
+    public static String text = "Стїґйф, Бм уствґї";
+    public static int key = 2;
+    public static Operation operation = Operation.ENCRYPT;
+
     public static void main(String[] args) {
-       Alphabet alphabet = new Alphabet();
-        System.out.println(alphabet.getAlphabetEn());
-        System.out.println(alphabet.getAlphabetUk());
+       Cipher cipher = new Cipher();
+       String textnew = "";
+       if (operation == Operation.DECRYPT){
+           textnew = cipher.getEncrypt(text, key);
+           System.out.println(textnew);
+       } else if (operation == Operation.ENCRYPT) {
+           textnew = cipher.getDecrypt(text, key);
+           System.out.println(textnew);
+       } else if (operation == Operation.BRUTE_FORCE) {
+           throw new UnsupportedOperationException("late");
+       }
+
     }
 
 }
